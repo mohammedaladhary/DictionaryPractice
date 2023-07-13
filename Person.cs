9c18e8aa247e3509a92b7ab7030aabc9b11accaa
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-13 12:13:59 +0400

    creating dictionary and print the changes

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPractice
+{
+    internal class Person
+    {
+        public long id {  get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+
+        public Person(long id, string name, string gender)
+        {
+            this.id = id;
+            this.name = name;
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return ($"name: {name} gender: {gender}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DictionaryPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Person person1 = new Person(1283, "Said", "Male");
+            Person person2 = new Person(1827, "Ahmed", "Female");
+            Person person3 = new Person(2938, "Hamed", "Male");
+
+            Dictionary<long, Person> persons = new Dictionary<long, Person>();
+            //persons.Add(person1.id, person1);
+            //persons.Add(person2.id, person2);
+            //persons.Add(person3.id, person3);
+
+            persons[person1.id] = person1;
+            persons[person2.id] = person2;
+            persons[person3.id] = person3;
+
+            person1.gender = "Female";
+            persons[person1.id] = person1;
+            //persons.TryAdd(person1.id, person1);
+            Person? result = null;
+            if(persons.TryGetValue(person1.id, out result))
+            {
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
